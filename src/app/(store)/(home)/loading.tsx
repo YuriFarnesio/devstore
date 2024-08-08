03ae0a2ebd7a5f56b6f13a2537c8bf776ecbdff9
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-08-08 00:31:25 -0300

    feat: add loading to home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,7 +1,12 @@
+import { Skeleton } from '@/components/skeleton'
+
 export default function HomeLoading() {
   return (
-    <div>
-      <p>Carregando...</p>
+    <div className="flex-1 grid grid-rows-6 grid-cols-9 gap-6">
+      <Skeleton className="row-span-6 col-span-6" />
+
+      <Skeleton className="row-span-3 col-span-3" />
+      <Skeleton className="row-span-3 col-span-3" />
     </div>
   )
 }
