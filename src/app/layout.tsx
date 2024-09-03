8e87e64032f2be591e4d6e51eec3696298e5b8d7
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-03 16:31:36 -0300

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { env } from '@/env'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -5,9 +6,16 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL(env.APP_URL),
+  alternates: {
+    canonical: '/',
+  },
   title: {
     template: '%s | devstore',
     default: 'devstore',
+  },
+  openGraph: {
+    images: 'icon.png',
   },
 }
 
